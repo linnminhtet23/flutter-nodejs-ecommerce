da1bc60c622d406466ee545873630fc7a67a3c1e
Linn Min Htet <linnminhtet23@gmail.com>
2022-08-31 00:33:20 +0630

    changes added

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:eshop/constants/global_variables.dart';
+import 'package:eshop/features/auth/screens/auth_screen.dart';
+import 'package:eshop/router.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+          colorScheme: const ColorScheme.light(
+            primary: GlobalVariables.secondaryColor,
+          ),
+          appBarTheme: const AppBarTheme(
+              elevation: 0,
+              iconTheme: IconThemeData(
+                color: Colors.black,
+              ))),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: const AuthScreen()
+    );
+  }
+}
